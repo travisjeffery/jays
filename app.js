fc7bded49d8bd643721acb0f22c77a4a803749dc
Travis Jeffery <tj@travisjeffery.com>
2015-09-18 23:49:47 +0000

    show next game

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ let handlebars = require('koa-handlebars');
 let path = require('path');
 let staticCache = require('koa-static-cache');
 let debug = require('debug')('app');
+let Promise = require('bluebird');
+let parse = Promise.promisify(require('csv-parse'));
+let fs = require('fs');
+let csv = fs.readFileSync('./schedule.csv', 'utf8');
+let schedule = null;
 
 const port = (process.env.PORT || 3000);
 const url = 'https://www.reddit.com/r/mlbstreams';
@@ -14,6 +19,28 @@ const url = 'https://www.reddit.com/r/mlbstreams';
 app.use(function*(next){
   if (this.url == '/health')
     return this.status = 200;
+  yield next;
+});
+
+app.use(function*(next){
+  let game = schedule[0];
+  let day = new Date(game['START DATE']);
+
+  schedule.forEach(function(g){
+    let start = new Date(g['START DATE']);
+    if (start < day) {
+      game = g;
+      day = start;
+    }
+  });
+
+  this.nextGame = {
+    day: game['START DATE'],
+    time: game['START TIME ET']
+  };
+
+  debug('game: %j', game)
+
   yield next;
 });
 
@@ -29,17 +56,27 @@ app.use(handlebars({
 app.use(function*(){
   let links = yield getLinks();
 
+  console.dir(this.app.context.schedule);
+
   debug('got links: %j', links);
 
   yield this.render("index", {
     links: links,
-    isGame: links.length > 0
+    isGame: false && links.length > 0,
+    nextGame: this.nextGame
   });
 });
 
 debug(`booting app at :${port}`);
 
-app.listen(port);
+parse(csv, { columns: true })
+  .then(function(data){
+    schedule = data;
+    app.listen(port);
+  })
+  .catch(function(err) {
+    throw err;
+  });
 
 /**
  * Scrape reddit for jays streams.
